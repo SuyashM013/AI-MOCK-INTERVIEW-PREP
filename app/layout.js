b221f860afd30ab64364bbd1e3a15e6fb835531b
Suyash <mishrasuyash013@gmail.com>
2025-05-17 22:47:52 +0530

    Raz_Upd

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
         <head>
           <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-          {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
         </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
