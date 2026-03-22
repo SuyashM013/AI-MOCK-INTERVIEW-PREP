9e45f9af424a829c79651dbf74f8a9d2b4d26397
Suyash <mishrasuyash013@gmail.com>
2026-03-22 13:12:12 +0530

    Quality Changes

--- app/dashboard/_components/InterviewCard.jsx
+++ app/dashboard/_components/InterviewCard.jsx
@@ -14,28 +14,9 @@ function InterviewCard({ interview }) {
         router.push('/dashboard/Interview/' + interview?.mockId + '/feedback')
     }
 
-    // const onDelete = async () => {
-    //     try {
-    //         const res = await fetch(`/api/Delete-interview?mockId=${interview?.mockId}`, {
-    //             method: 'DELETE',
-    //         });
-
-    //         const data = await res.json();
-
-    //         if (!res.ok) {
-    //             throw new Error(data.error || "Delete failed");
-    //         }
-
-    //         console.log(data);
-    //         router.refresh();
-
-    //     } catch (err) {
-    //         console.error("Error deleting interview:", err);
-    //     }
-    // };
 
     return (
-        // <div className='border border-blue-200 hover:shadow-md bg-blue-50 shadow-sm rounded-lg p-3'>
+
         <div className=''>
 
             <h2 className='font-bebas text-3xl  text-primary uppercase'> {interview.jobPosition}</h2>
@@ -49,7 +30,7 @@ function InterviewCard({ interview }) {
                 <Button onClick={onStart} className='w-full 
                 font-chau'>Start</Button>
 
-                {/* <Button id="delete" onClick={onDelete} className='w-full font-chau bg-red-500'>Delete</Button> */}
+                
 
             </div>
 
